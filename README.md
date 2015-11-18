6ae83a53614f252dca7fbc8eb7c1ff943496e930
benholloway <ben.holloway@3plearning.com>
2015-11-18 14:36:31 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# invocation-rate-compliance
+# Invocation Rate Compliance
+
+[![NPM](https://nodei.co/npm/invocation-rate-compliance.png)](http://github.com/bholloway/invocation-rate-compliance)
+
 A throttle utility that ensures invocation complies to a limited rate
+
+## Usage
+
+```javascript
+var invocationRateCompliance = require('invocation-rate-compliance');
+
+var throttled = invocationRateCompliance({
+  delay        : 100,
+  before       : function before() {
+      return 'invocation was';
+    },
+  compliant    : function compliant(previous) {
+      return previous + ' compliant';
+    },
+  antiCompliant: function antiCompliant(previous) {
+      return previous + ' anti-compliant';
+    },
+  after        : function after(previous, x) {
+      return previous + ' with argument ' + String(x);
+    }
+});
+
+throttled(5);                // invocation was compliant with argument 5
+throttled('a');              // invocation was anti-compliant with argument a
+setTimeout(throttled, 200);  // invocation was compliant with argument undefined
+
+```
+
+The utility is a factory for a method that may be invoked. The first invocation is always **compliant** and a chain of methods `[before, compliant, after]` is called. If the method is called again before the given delay then invocation is **anti-compliant** and a chain of methods `[before, antiCompliant, after]` is called.
+
+The delay is milliseconds measured from the last compliant invocation. Anti-compliant invocations do not change internal state.
+
+At each stage in the method chain the value returned by the previous function is the first argument to the following function. Successive arguments will match those of the invocation. The first argument to the `before` method is always `undefined`.
---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * A utility that ensures invocation of a function complies to a limited rate.
+ *
+ * The utility is a factory for a method that may be invoked. The first invocation is always `compliant` and a chain of
+ * methods `[before, compliant, after]` is called. If the method is called again before the given delay then invocation
+ * is `anti-compliant` and a chain of methods `[before, antiCompliant, after]` is called.
+ *
+ * The delay is milliseconds measured from the last compliant invocation. Anti-compliant invocations do not change
+ * internal state.
+ *
+ * At each stage in the method chain the value returned by the previous function is the first argument to the following
+ * function. Successive arguments will match those of the invocation. The first argument to the `before` method is
+ * always `undefined`.
+ *
+ * @param {{delay:number, before:function, after:function, compliant:function, antiCompliant:function}} options
+ * @returns {function} A function that will invoke the method chain
+ */
+function onThrottled(options) {
+  options = options || {};
+  var delay         = options.delay || 0,
+      before        = options.before,
+      compliant     = options.compliant,
+      antiCompliant = options.antiCompliant,
+      after         = options.after;
+  var nextTime;
+
+  return function instance() {
+    var args        = Array.prototype.slice.call(arguments),
+        now         = Date.now(),
+        isCompliant = !nextTime || (now > nextTime),
+        methodChain = [before, (isCompliant ? compliant : antiCompliant), after];
+
+    if (isCompliant) {
+      nextTime = now + delay;
+    }
+    return methodChain.reduce(eachMethod, undefined);
+
+    function eachMethod(previousValue, method) {
+      return (typeof method === 'function') ? method.apply(null, [previousValue].concat(args)) :
+        (typeof method !== 'undefined') ? method : previousValue;
+    }
+  };
+}
+
+module.exports = onThrottled;
